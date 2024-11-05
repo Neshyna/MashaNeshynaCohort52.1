5f55f6b3817ea69b78897ad9241b1010959f9290
Neshyna <mariya.neshyna@gmail.com>
2024-11-05 15:31:06 +0100

    lesson 39 +

--- src/hw39/Main.java
+++ src/hw39/Main.java
@@ -7,7 +7,12 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void retainLists(){
+        /*
+        Task 1
+        Напишите метод, который принимает на вход две реализации интерфейса List.
+        Метод должен возвращать список состоящий из элементов, которые присутствуют в обеих коллекциях.
+         */
 
         List<Integer> c1 = new ArrayList<>(java.util.List.of(1,2,3,4,5));
         List<Integer> c2 = new LinkedList<>(java.util.List.of(4,5,6,7,8));
@@ -17,5 +22,11 @@ public class Main {
 
         c1.retainAll(c2);
         System.out.println("c1.retain(c2): " + c1);
+    }
+
+    public static void main(String[] args) {
+
+        retainLists();
+
     }
 }
