991f77fdc8989bb2b0529b18739f6a70e3741a2f
Neshyna <mariya.neshyna@gmail.com>
2024-11-06 22:08:14 +0100

    hw40

--- src/hw39/Main.java
+++ src/hw39/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Integer> c1 = new ArrayList<>(java.util.List.of(1,2,3,4,5));
+        List<Integer> c1 = new LinkedList<>(java.util.List.of(1,2,3,4,5));
         List<Integer> c2 = new LinkedList<>(java.util.List.of(4,5,6,7,8));
         System.out.println("c1" + c1);
         System.out.println("c2 " + c2);
---
+++ src/hw40/Task1.java
@@ -0,0 +1,55 @@
+package hw40;
+
+import java.util.*;
+
+public class Task1 {
+    /*
+    Task 1
+    Напишите метод, принимающий строку и возвращающий Список ее слов без повторений,
+    отсортированный в порядке увеличения длин слов.
+    Если строки имеют одинаковую длину - сортировать в естественном порядке
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println(wordsList("Test text to return words list without duplicates sorted from smallest word to longest word"));
+    }
+
+    public static List<String> wordsList(String phrase ){
+
+        String testPhrase = phrase.replaceAll("[^ a-zA-Z0-9a-яА-Я]", " ");
+
+        String[] arrayWords = testPhrase.split(" ");
+        System.out.println("arrayWords: " + arrayWords);
+
+        Comparator<String> wordLengthComp = new Comparator<String>() {
+            @Override
+            public int compare(String w1, String w2) {
+                int wordLength = w1.length() - w2.length();
+                if (wordLength == 0){
+                    return w1.compareTo(w2);//natural order lexicographically
+                };
+                return wordLength;
+            }
+        };
+
+        Arrays.sort(arrayWords, wordLengthComp);
+        System.out.println("compared array:");
+        System.out.println(Arrays.toString(arrayWords));
+
+        List<String> listWords = Arrays.asList(arrayWords);
+        System.out.println("listWords: " + listWords);
+
+        List<String> listWithoutDuplicates = getUniqueList(listWords);
+        System.out.println("list without duplicates: " + listWithoutDuplicates);
+
+        return listWithoutDuplicates;
+    }
+    public static <T> List<T> getUniqueList(List<T> list){
+        //return set, with only unique elements
+        Set<T> set = new LinkedHashSet<>(list);
+        //return new list with all set elements
+        return new ArrayList<>(set);
+
+    }
+}
